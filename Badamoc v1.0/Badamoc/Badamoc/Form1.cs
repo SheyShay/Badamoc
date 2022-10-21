3cc9d06b3197295b4d93772a515aa826b7ebb329
David Lansonneur <95921154+SheyShay@users.noreply.github.com>
2022-10-21 14:56:01 +0200

    Add files via upload

---
+++ Badamoc v1.0/Badamoc/Badamoc/Form1.cs
@@ -0,0 +1,50 @@
+namespace Badamoc
+{
+    public partial class Connexion : Form
+    {
+        public Connexion()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Connexion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
